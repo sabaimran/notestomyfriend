2c475ae0a95d6f810947be27acef465340f5d714
Saba Imran <narmiabas@gmail.com>
2020-12-25 20:16:06 -0800

    Added some actual test data, re-styled the text blocks

--- data/notes.js
+++ data/notes.js
@@ -1,18 +1,18 @@
 const notes = 
     [
-        {
-            author: "saba",
-            date: "2020-04-01T00:00:00",
-            text: "hi, what's up?"
-        },
         { 
             author: "q",
-            date: "2020-04-02T00:00:00",
-            text: "not much :p"
+            date: "2020-11-09T00:00:00",
+            text: "i read this thing\nand i'm listening to this thing\nand i'm feeling strangely mellow\nlike that idealistic mellow i want to bike and bike and bike and\ngo away\nbe a bit reckless, have no obligations, fall into a body of unending water, fill my lungs with air"
         },
         { 
             author: "saba",
-            date: "2020-05-02T00:00:00",
-            text: "this city on a hill does not belong"
+            date: "2020-04-29T00:00:00",
+            text: "SO I just went bouldering and I forgot to take a picture of the gym before leaving but I finished some 5a tracks!! So proud of myself hahah\nThank you back to the times where you took me rock climbing\nBut geez still there're some tracks I look at (most tracks), and I'm like how do people do that?"
+        },
+        {
+            author: "saba",
+            date: "2020-11-15T00:00:00",
+            text: "this tent (on AirBnb) is over 1k per month, which is cheaper than our Urbana rent, think about that"
         }
     ];
--- scripts/main.js
+++ scripts/main.js
@@ -9,20 +9,24 @@ function grid() {
 
         const note = notes[i];
 
+        // Construct (parent) row
         var row = document.createElement('div');
         row.className = "text-"+note.author+" text-row";
         row.id = "text" + i;
 
+        // Add author element
         var author = document.createElement('div');
         author.innerHTML = note.author;
         author.className = "text-author";
         row.appendChild(author);
 
+        // Add the text data
         var text = document.createElement('div');
-        text.innerHTML = note.text;
+        text.innerHTML = "<pre>" + note.text + "</pre>";
         text.className = "text-data";
         row.appendChild(text);
 
+        // Add the date
         var date = document.createElement('div');
         date.innerHTML = getPrettyDate(note.date);
         date.className = "text-date";
@@ -38,8 +42,6 @@ function getPrettyDate(date) {
     var parsedDate = new Date(Date.parse(date));
     return `${parsedDate.getFullYear()}-${parsedDate.getMonth()+1}-${parsedDate.getDate()}`;
 }
-
-console.log(grid());
 
 // and you should use 
 document.getElementById('notes-container').appendChild(grid());
