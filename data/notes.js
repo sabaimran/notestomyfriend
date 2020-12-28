0a2a932a5669b2d3956e53ab09509fd7049d197b
Kathleen Hu <khu.qian@gmail.com>
2020-12-28 12:47:14 -0800

    saving more data

--- data/notes.js
+++ data/notes.js
@@ -1,9 +1,29 @@
 const notes = 
     [
+         { 
+            author: "q",
+            date: "2019-12-06T09:45:00",
+            text: "Feeling a bit immobile. The answer is just sleep. When life is so fucking full. This is all constructed and our energy is impossible to allocate optimized \n Kind of like holy freak \n We live on earth"
+         },
+         { 
+            author: "saba",
+            date: "2014-01-12T09:45:00",
+            text: "the saying of the same thing twice in different words, generally considered to be a fault of style (e.g., they arrived one after the other in succession ). \n smilar to pleonasm"
+         },
+         { 
+            author: "q",
+            date: "2014-01-12T08:32:00",
+            text: "what does 'tautology' mean? \n it sounds like an amazing word"
+        },
+        { 
+            author: "saba",
+            date: "2019-12-09T00:00:00",
+            text: "Remove your ego from it. What happens happens, just make decisions that make you feel like you could look back and be proud of what you’ve done."
+        },
         { 
             author: "saba",
             date: "2013-12-31T00:00:00",
-            text: "i think people just want to be more practical. and get money. which fucking sucks. humans are unique in that they can think and learn, as opposed to other animals that don't seem to be able to (to this degree). and it APPEARS to be that kids are just going into STEM jobs because that's where they'll be "successful." but they're sacrificing growth in what they may be passionate about in the process. so, at what cost do you have the nice job and the nice family and the nice house? would it still be worth it to give up what you care about? i don't know. some people are just lucky that they're good at what they do and they genuinely like it and society thinks that what they're good at is valuable (engineering/medicine). others may really love something and not pursue it because society doesn't place as much importance on it (studies of the classics/anthropology). idk, i wish we lived in a society that encouraged learning for the sake of learning. but we don't, and it is what it is, right?"
+            text: "i think people just want to be more practical. and get money. which fucking sucks. humans are unique in that they can think and learn, as opposed to other animals that don't seem to be able to (to this degree). and it APPEARS to be that kids are just going into STEM jobs because that's where they'll be 'successful.' but they're sacrificing growth in what they may be passionate about in the process. so, at what cost do you have the nice job and the nice family and the nice house? would it still be worth it to give up what you care about? i don't know. some people are just lucky that they're good at what they do and they genuinely like it and society thinks that what they're good at is valuable (engineering/medicine). others may really love something and not pursue it because society doesn't place as much importance on it (studies of the classics/anthropology). idk, i wish we lived in a society that encouraged learning for the sake of learning. but we don't, and it is what it is, right?"
         },
         { 
             author: "q",
