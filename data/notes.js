f3fb2c3aea7e0de6616fe35d871b5a8904d3532e
Saba <narmiabas@gmail.com>
2021-09-16 08:26:27 -0700

    Replace saba with s in author

--- data/notes.js
+++ data/notes.js
@@ -7,7 +7,7 @@ const notes =
             tag: "angst"
         },
         { 
-            author: "saba",
+            author: "s",
             date: "2014-01-12T09:45:00",
             text: "the saying of the same thing twice in different words, generally considered to be a fault of style (e.g., they arrived one after the other in succession ). \nsimilar to pleonasm"
         },
@@ -18,13 +18,13 @@ const notes =
             tag: "romance"
         },
         { 
-            author: "saba",
+            author: "s",
             date: "2019-12-09T00:00:00",
             text: "Remove your ego from it. What happens happens, just make decisions that make you feel like you could look back and be proud of what you've done.",
             tag: "self-improvement"
         },
         { 
-            author: "saba",
+            author: "s",
             date: "2013-12-31T00:00:00",
             text: "i think people just want to be more practical. and get money. which fucking sucks. humans are unique in that they can think and learn, as opposed to other animals that don't seem to be able to (to this degree). and it APPEARS to be that kids are just going into STEM jobs because that's where they'll be 'successful.' but they're sacrificing growth in what they may be passionate about in the process. so, at what cost do you have the nice job and the nice family and the nice house? would it still be worth it to give up what you care about? i don't know. some people are just lucky that they're good at what they do and they genuinely like it and society thinks that what they're good at is valuable (engineering/medicine). others may really love something and not pursue it because society doesn't place as much importance on it (studies of the classics/anthropology). idk, i wish we lived in a society that encouraged learning for the sake of learning. but we don't, and it is what it is, right?",
             tag: "angst"
@@ -36,13 +36,13 @@ const notes =
             tag: "ephemeral"
         },
         { 
-            author: "saba",
+            author: "s",
             date: "2020-04-29T00:00:00",
             text: "SO I just went bouldering and I forgot to take a picture of the gym before leaving but I finished some 5a tracks!! So proud of myself hahah\nThank you back to the times where you took me rock climbing\nBut geez still there're some tracks I look at (most tracks), and I'm like how do people do that?",
             tag: "self-improvement"
         },
         {
-            author: "saba",
+            author: "s",
             date: "2020-11-15T00:00:00",
             text: "this tent (on AirBnb) is over 1k per month, which is cheaper than our Urbana rent, think about that",
             tag: "college"
@@ -60,18 +60,18 @@ const notes =
             tag: "self-improvement"
         },
         { 
-            author: "saba",
+            author: "s",
             date: "2020-10-23T00:00:00",
             text: "You know people are just story books\nEveryone is just rolling through their own plot lines"
         },
         { 
-            author: "saba",
+            author: "s",
             date: "2020-10-14T00:00:00",
             text: "What is breathing?\nA cloudy, windy night is still bright enough to see in the dark\nI smell the scent of leaves decaying\nA new birth is underway\nI hope it's like us. All the cuts and bruises we heal, I hope it makes us new and tough. It takes away our softness though. Young and supple, like a souffl&#233 that crumples. Ashes make for fertile ground.",
             tag: "youth,travel"
         },
         { 
-            author: "saba",
+            author: "s",
             date: "2015-07-15T00:00:00",
             text: "I can't see the words. They're not on paper they're not on the screen they're not digitized or permanently etched in any surface. They're not even words, they're thoughts. Untranslated not complex not complicated just existing like thoughts on brain like plastic wrap",
             tag: "angst,ephemeral"
